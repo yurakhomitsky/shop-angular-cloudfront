cb62ad35d9d2f7fc2f044bdaa89e659cdcd3b35f
yurakhomitsky <yura.khomitsky8@gmail.com>
2024-03-25 13:54:27 +0200

    Add terraform
    Signed-off-by: Yurii Khomitskyi <ykhomitskyi@slb.com>

---
+++ main.tf
@@ -0,0 +1,34 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 3.92.0"
+    }
+  }
+
+  required_version = ">= 1.1.0"
+}
+
+provider "azurerm" {
+  features {}
+}
+
+
+resource "azurerm_resource_group" "front_end_rg" {
+  name     = "rg-frontend-sand-ne-001"
+  location = "northeurope"
+}
+
+resource "azurerm_storage_account" "front_end_sa" {
+  name                     = "stfrontend001"
+  location                 = azurerm_resource_group.front_end_rg.location
+
+  account_replication_type = "LRS"
+  account_tier             = "Standard"
+  account_kind             = "StorageV2"
+  resource_group_name      = azurerm_resource_group.front_end_rg.name
+
+  static_website {
+    index_document = "index.html"
+  }
+}
